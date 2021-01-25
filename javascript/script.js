2414be08bb2dc8af937d9f272ca08c8587cdf08e
Mitchell Gould <mitchellgould7@gmail.com>
2021-01-25 14:37:44 -0500

    people counter working

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,24 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+  const dec_btn = document.getElementById("dec");
+  const inc_btn = document.getElementById("inc");
+  const people_count = document.getElementById("people");
+  let count = parseInt(people_count.value);
+
+  dec_btn.addEventListener('click', (e)=> {
+    e.preventDefault();
+    if(count > 0) {
+      count --;
+      people_count.value = count;
+    }
+  })
+
+  inc_btn.addEventListener('click', (e)=> {
+    e.preventDefault();
+    if(count < 20) {
+      count ++;
+      people_count.value = count;
+    }
+  })
+
+});
